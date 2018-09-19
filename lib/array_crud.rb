eca121e9981a66d9501a615d981792b8aee2cd12
heather sterman <heather.sterman531@gmail.com>
2018-09-19 14:52:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = []
-  array.pop()
+  cats = ["fluffy", "garfield", "bub", "maru"]
+  cats.pop("maru")
 end
 
 def remove_element_from_start_of_array(array)

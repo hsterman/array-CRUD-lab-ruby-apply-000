22b65c8248adb78c891aa53af6072d88ea900415
heather sterman <heather.sterman531@gmail.com>
2018-09-19 14:49:08 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  elements = ["earth", "air", "water", "fire"]
+  four_elements = ["earth", "air", "water", "fire"]
 end
 
 def add_element_to_end_of_array
